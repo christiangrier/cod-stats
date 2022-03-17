6eb46316002eb65031331f585fcfc31bdd02829c
christiangrier <62674125+christiangrier@users.noreply.github.com>
2022-03-17 21:52:31 +1100

    adding screenshot capture

--- textreader.py
+++ textreader.py
@@ -4,8 +4,28 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-IMAGE_PATH = 'images/double.png'
-reader = easyocr.Reader(['en'])
-result = reader.readtext(IMAGE_PATH)
+def gen_Img(video):
+    # Screen captures of the provided video based on a desired location and timing
+    index = 0
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
 
-print(result)
+
+
+
+
+def gen_Text():
+    # Image to text
+    IMAGE_PATH = 'images/double.png'
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(IMAGE_PATH)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    gen_Img()
+    # need to pass something here^, havent made that yet
+    gen_Text()
